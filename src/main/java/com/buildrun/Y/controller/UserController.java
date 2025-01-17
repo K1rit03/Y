665b9befaaede4bf0302799d29b78d7f63cdce32
Thiago <thiagodeboliveira@outlook.com>
2025-01-17 15:11:24 -0300

    get stuck at some stupid bug with the authorization of the user requisition

--- src/main/java/com/buildrun/Y/controller/UserController.java
+++ src/main/java/com/buildrun/Y/controller/UserController.java
@@ -10,8 +10,10 @@ import lombok.AllArgsConstructor;
 import lombok.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -22,20 +24,22 @@ import java.util.Set;
 @AllArgsConstructor
 @RestController
 public class UserController {
+
     private final UserRepository userRepository;
-
     private final RoleRepository roleRepository;
+    private final BCryptPasswordEncoder passwordEncoder;
 
-    private final PasswordEncoder passwordEncoder;
+
 
     @Transactional
     @PostMapping("/users")
-    public ResponseEntity<Void> newUser(@RequestBody CreateUserDto dto){
+    public ResponseEntity<Void> newUser(@RequestBody CreateUserDto dto) {
 
         var basicRole = roleRepository.findByName(Role.Values.BASIC.name());
+
         var userFromDb = userRepository.findByUsername(dto.username());
-        if(userFromDb.isEmpty()){
-            throw new ResponseStatusException((HttpStatus.UNPROCESSABLE_ENTITY));
+        if (userFromDb.isPresent()) {
+            throw new ResponseStatusException(HttpStatus.UNPROCESSABLE_ENTITY);
         }
 
         var user = new User();
@@ -44,8 +48,8 @@ public class UserController {
         user.setRoles(Set.of(basicRole));
 
         userRepository.save(user);
-        return ResponseEntity.ok().build();
 
+        return ResponseEntity.ok().build();
     }
-    //commit thing
+
 }
