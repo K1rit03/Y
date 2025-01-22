18f7d54e23ff05e6f035931e2d605aa6b137396c
Thiago <thiagodeboliveira@outlook.com>
2025-01-22 10:57:55 -0300

    fixing the authorization bug

--- src/main/java/com/buildrun/Y/dto/CreateUserDto.java
+++ src/main/java/com/buildrun/Y/dto/CreateUserDto.java
@@ -3,7 +3,7 @@ package com.buildrun.Y.dto;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 
-public record CreateUserDto(
+public record   CreateUserDto(
         @NotBlank(message = "Username is required")
         @Size(min = 3, max = 50, message = "Username must be between 3 and 50 characters")
         String username,
